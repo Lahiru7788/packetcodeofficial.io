777e8aa0cc7234a56111805d08cdd8091eb28345
Lahiru7788 <121952900+Lahiru7788@users.noreply.github.com>
2023-03-08 01:27:50 +0530

    Add new file

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multipletext",{
+    strings: ["Web Developer","Frontend Developer","Backend Developer","Android Developer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay:1000,
+    loop: true
+
+})
